1f2d6b0b06f66ba442994b674421260dfc059fa5
Theoderic <onipetheoderic@gmail.com>
2020-04-28 21:48:44 +0100

    all

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import {createAppContainer} from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import DescriptionScreen from './src/screens/DescriptionScreen'
+import DescriptionScreen from './src/screens/DescriptionScreen';
+import SplashScreen from './src/screens/SplashScreen';
+
 
 import HomeScreen from './src/screens/HomeScreen';
 import SelectedProductScreen from './src/screens/SelectedProductScreen';
 const AppNavigator = createStackNavigator(
   {
+    SplashScreen:SplashScreen,
     HomeScreen:HomeScreen,
     SelectedProductScreen:SelectedProductScreen,
     DescriptionScreen:DescriptionScreen
